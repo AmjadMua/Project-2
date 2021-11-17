5c28c97e9a256d8d154cf00945fda2f64fe8e900
AmjadMua <92171759+AmjadMua@users.noreply.github.com>
2021-11-17 15:02:12 +0300

    Changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
 import Navbar from "./components/Navbar"
 
 function App() {
   return <Navbar />
-=======
-import logo from "./logo.svg"
-import "./App.css"
-
-function App() {
-  return <div className="App"></div>
->>>>>>> Stashed changes
 }
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import { Navbar, Container, Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import NewsLogo from "../images/NewsLogo.png"
 
 function NavbarItem() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>News Journal</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={NewsLogo} width="110" height="60" className="d-inline-block align-top" alt="React Bootstrap logo" />
+        </Navbar.Brand>
+        <Navbar.Brand> News Journal</Navbar.Brand>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Link to="/">Features</Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to="/">Pricing</Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
