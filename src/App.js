4b9d5909c7202d855b1aa34da82939e5fc4a8c8a
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-11-17 14:45:19 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+<<<<<<< Updated upstream
 import Navbar from "./components/Navbar"
 
 function App() {
   return <Navbar />
+=======
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return <div className="App"></div>
+>>>>>>> Stashed changes
 }
 
 export default App
